37a3e16b2c862b2f1c26e919bff2da4b1e54d4be
iarif99 <ariful.jnu13@gmail.com>
2026-03-10 00:07:45 +0600

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => e.waitUntil(self.skipWaiting()));
-
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => new Response('Offline Mode Active. Please connect to the internet.'))
-  );
-});
